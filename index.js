afa1fa9db068ec6c9d22f591144111f584c1cc77
Minhyuk Kim <minhyukkim@Minhyuks-MacBook-Air.local>
2018-02-07 01:19:40 +0900

    Completed Version

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var miio = require('miio');
 var Service, Characteristic;
 
 module.exports = function (homebridge) {
+  
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
 
@@ -20,10 +21,9 @@ function XiaoMiPowerStrip(log, config) {
     this.switchService = new Service.Switch(this.name);
 
     this.switchService
-        .getCharacteristic(Characteristic.Active)
-        .on('get', this.getPowerState.bind(this))
-        .on('set', this.setPowerState.bind(this));
-
+       .getCharacteristic(Characteristic.On)
+       .on('get',this.getPowerState.bind(this))
+       .on('set',this.setPowerOn.bind(this));
 
     this.services.push(this.switchService);
 
@@ -36,81 +36,43 @@ function XiaoMiPowerStrip(log, config) {
 
     this.services.push(this.serviceInfo);
 
-
     this.discover();
 }
 
 XiaoMiPowerStrip.prototype = {
     discover: function () {
         var accessory = this;
-
         const device = miio.createDevice({
-            address: '192.168.1.23',
-            token: '07642c5604a7ab99ebb33e5ec1e2f68f',
-            model: "zimi.powerstrip.v2"
+            address: accessory.address,
+            token: accessory.token,
+            model: accessory.model
         });
         device.init();
         accessory.device = device;
+        device.power();
     },
 
     getPowerState: function (callback) {
-        var state = this.device.power
-        if (state) {
-            callback(null, Characteristic.Active.ACTIVE);
+        var on = this.device.power()
+        if (on) {
+            callback(null, true);
         } else {
-            callback(null, Characteristic.Active.INACTIVE);
+            callback(null, false);
         }
-        this.log.info('getPowerState:', state);
+        this.log.info('getPowerState:', on);
     },
 
-    setPowerState: function (Power, callback) {
+    setPowerOn: function (Power, callback) {
         this.log.info('setPowerState:', Power);
-        this.device.setPower(state === Characteristic.Active.ACTIVE)
-            .then((res) => {
-                this.log.info('setPowerStateResponse:', res);
-                callback();
-            });
+        if(!Power){
+          this.device.setPower(false)
+            .then(on => console.log('Power is now', on));
+        } else{
+          this.device.setPower(true)
+            .then(on => console.log('Power is now', on));
+        }
+        callback();
     },
-    //
-    // getCurrentAirPurifierState: function (callback) {
-    //     var mode = {
-    //         idle: Characteristic.CurrentAirPurifierState.INACTIVE,
-    //         silent: Characteristic.CurrentAirPurifierState.IDLE,
-    //         favorite: Characteristic.CurrentAirPurifierState.PURIFYING_AIR,
-    //         auto: Characteristic.CurrentAirPurifierState.PURIFYING_AIR,
-    //         low: Characteristic.CurrentAirPurifierState.IDLE,
-    //         medium: Characteristic.CurrentAirPurifierState.PURIFYING_AIR,
-    //         high: Characteristic.CurrentAirPurifierState.PURIFYING_AIR,
-    //     };
-    //     var state = mode[this.device.mode];
-    //     this.log.info('CurrentAirPurifierState:', this.device.mode, state);
-    //     callback(null, state);
-    // },
-    //
-    // getTargetAirPurifierState: function (callback) {
-    //     var mode = {
-    //         idle: Characteristic.TargetAirPurifierState.MANUAL,
-    //         silent: Characteristic.TargetAirPurifierState.MANUAL,
-    //         favorite: Characteristic.TargetAirPurifierState.MANUAL,
-    //         auto: Characteristic.TargetAirPurifierState.AUTO,
-    //         low: Characteristic.TargetAirPurifierState.MANUAL,
-    //         medium: Characteristic.TargetAirPurifierState.MANUAL,
-    //         high: Characteristic.TargetAirPurifierState.MANUAL,
-    //     };
-    //     var state = mode[this.device.mode];
-    //     this.log.info('TargetAirPurifierState:', this.device.mode, state);
-    //     callback(null, state);
-    // },
-    //
-    // setTargetAirPurifierState: function (state, callback) {
-    //     if (state === Characteristic.TargetAirPurifierState.MANUAL) {
-    //         this.device.setMode('favorite');
-    //     } else if (state === Characteristic.TargetAirPurifierState.AUTO) {
-    //         this.device.setMode('auto');
-    //     }
-    //     callback();
-    // },
-
 
     identify: function (callback) {
         callback();
